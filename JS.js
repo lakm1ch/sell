0c0df17b451250c3c4771d9068c0ea048552b50c
lakm1ch <130504423+lakm1ch@users.noreply.github.com>
2024-12-09 20:02:34 +0100

    Initial commit

---
+++ JS.js
@@ -0,0 +1,8 @@
+// Ждем загрузки страницы
+window.addEventListener('load', () => {
+    // Задержка перед переключением
+    setTimeout(() => {
+        // Перенаправление на index.html
+        window.location.href = 'index.html';
+    }, 2000); // Задержка в миллисекундах (2 секунды)
+});
